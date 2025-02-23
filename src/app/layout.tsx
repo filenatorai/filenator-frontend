4d62f3765e60c0421dfc3b1af8bffe474cc3cbf1
vikram.sengar <vikram123sengar@gmail.com>
2025-02-24 02:29:03 +0530

    Initial Changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
-import { Navbar } from "@/components/Navbar"
+// import { Navbar } from "@/components/Navbar"
 import "@/styles/globals.css"
+import Navbar from "@/components/Navbar"
+
 
 export const metadata: Metadata = {
   title: 'FileHub',
