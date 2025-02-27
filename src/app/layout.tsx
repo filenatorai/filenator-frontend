bf8e9ca75760759f740b3b9199c24f951c024576
vikram.sengar <vikram123sengar@gmail.com>
2025-02-28 00:50:56 +0530

    footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 // import { Navbar } from "@/components/Navbar"
 import "@/styles/globals.css"
 import Navbar from "@/components/Navbar"
+import Footer from '@/components/Footer'
 
 
 export const metadata: Metadata = {
@@ -21,6 +22,7 @@ export default function RootLayout({
         <main>
           {children}
         </main>
+        <Footer />
       </body>
     </html>
   )
